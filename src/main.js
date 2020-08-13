0878306d46bc21dc8b3cd25997169e4ca72431db
Soehtun <soehtuncu@gmail.com>
2020-08-13 10:30:17 +0630

    Vuejs connect with firebase database

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import firebase from 'firebase'
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBduRRjmkErpa628o4ZkBkCSSSkc0YDTiw",
+  authDomain: "testdatabse-a2136.firebaseapp.com",
+  databaseURL: "https://testdatabse-a2136.firebaseio.com",
+  projectId: "testdatabse-a2136",
+  storageBucket: "testdatabse-a2136.appspot.com",
+  messagingSenderId: "180356959582",
+  appId: "1:180356959582:web:c07ea2df93e0fe0d6d53cf",
+  measurementId: "G-SWRFK08LRB"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+window.firebase = firebase;
 new Vue({
   router,
   render: h => h(App)
---
+++ src/views/dbtest.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <h1>Names</h1>
+    <el-input style="width:250px" id="name" v-model="namegp" placeholder="Press enter key to add name" @keyup.enter.native="addname()"></el-input>
+    <ul>
+      <li v-for="(username,key) in names" :key="key">
+        <h3>{{username.name}}</h3>
+        <!--<template slot-scope="scope">
+          <el-button type="primary" v-if="VisibleEdit" @click="edit(scope.$index, names)">Edit</el-button>
+        
+        </template> -->
+        <el-button type="primary" v-if="VisibleEdit" @click="edit()">Edit</el-button>
+        <el-button type="primary" v-if="VisibleSave" @click="save(key)">Save</el-button>
+        <el-button type="danger" @click="deletename(key)">Delete</el-button>
+        <br />
+        <br />
+
+        <el-input style="width:250px" v-if="VisibleInput" v-model="editForm[key]" @keyup.enter.native="edit(key)"></el-input>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+export default {
+  data() {
+    return {
+      namegp: "",
+      names: [],
+      editForm: [],
+      VisibleSave: false,
+      VisibleEdit:true,
+      VisibleInput: false
+    };
+  },
+  methods: {
+    addname() {
+      firebase
+        .database()
+        .ref("names")
+        .push({ name: this.namegp })
+        .then(data => {
+          console.log(data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+        this.namegp =''
+    },
+    edit() {
+      this.VisibleSave= true;
+      this.VisibleEdit= false;
+      this.VisibleInput = true;
+      //this.editedRow = row[index];
+      //this.editForm = this.editedRow.editForm
+    },
+    save(key){
+      this.VisibleSave = false;
+      this.VisibleEdit =true;
+      this.VisibleInput = false;
+      this.namegp='';
+      firebase
+        .database()
+        .ref("names/" + key)
+        .set({
+          name: this.editForm[key]
+        });
+    },
+    deletename(key) {
+      firebase
+        .database()
+        .ref("names/" + key)
+        .remove();
+    }
+  },
+  created() {
+    firebase
+      .database()
+      .ref("names")
+      .on("value", snapshot => {
+        this.names = snapshot.val();
+      });
+  }
+};
+</script>
+
+<style scoped>
+li{
+  list-style-type: none;
+}
+</style>
